e226978424ed32318f00fa8a65515815d84ff6e0
Kristo Aun <kristoaun@live.com>
2024-01-22 17:03:52 +0200

    module

--- filesystem/mnt/install.sh
+++ filesystem/mnt/install.sh
@@ -7,4 +7,6 @@ apt-get install -y vim htop curl openssh-server
 ssh-keygen -t rsa -b 4096 -q -N ""
 echo $SSH_AUTHORIZED_KEYS | base64 --decode > ~/.ssh/authorized_keys
 
+# service ssh start
+
 sh /mnt/setup-ca.sh
--- variables.tf
+++ variables.tf
@@ -1,82 +1,14 @@
-variable "cluster_domain" {
-  type    = string
-  default = "cluster.local"
-}
-
-variable "k3s_version" {
-  type    = string
-  default = "v1.28.5+k3s1"
-}
-
 variable "swarm_private_key" {
   type        = string
   description = "Base64 encoded private key PEM."
   sensitive   = true
 }
 
-variable "cidr_pods" {
-  type = string
-}
-
-variable "cidr_services" {
-  type = string
-}
-
-variable "k3s_install_env_vars" {
-  type    = map(any)
-  default = {}
-}
-
-variable "global_flags" {
-  type    = list(string)
-  default = []
-}
-
-variable "server_flags" {
-  type    = list(string)
-  default = []
-}
-
-variable "server_labels" {
-  type    = map(string)
-  default = {}
-}
-
-variable "worker_labels" {
-  type = map(string)
-  default = {
-  }
-}
-
-variable "managed_fields" {
-  type    = list(string)
-  default = ["label", "taint", "annotation"]
-}
-
-variable "use_sudo" {
-  type        = bool
-  description = "Whether or not to use kubectl with sudo during cluster setup."
-  default     = false
-}
-
-variable "drain_timeout" {
-  type    = string
-  default = "60s"
-}
-
-variable "containers_server" {
+variable "servers" {
   type = list(object({
     name         = string
     ipv4_address = string
   }))
-}
-
-variable "containers_worker" {
-  type = list(object({
-    name         = string
-    ipv4_address = string
-  }))
-  default = []
 }
 
 variable "network_name" {
@@ -85,17 +17,4 @@ variable "network_name" {
 
 variable "restart" {
   type = string
-}
-
-variable "exec_enabled" {
-  type    = bool
-  default = true
-}
-
-variable "image" {
-  type = string
-}
-
-variable "node_connection_timeout" {
-  default = "60s"
 }
