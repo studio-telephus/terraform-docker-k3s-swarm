ce3024855ba3be25c8bca064721eb409da00cd42
Kristo Aun <kristoaun@live.com>
2024-02-24 10:54:43 +0200

    swarm image

--- main.tf
+++ main.tf
@@ -11,20 +11,29 @@ locals {
   container_environment = {
     SSH_AUTHORIZED_KEYS = base64encode(data.tls_public_key.swarm_public_key.public_key_openssh)
   }
+  docker_image_name = "tel-debian-bookworm-systemd"
 }
 
-resource "docker_image" "docker_ubuntu_systemd" {
-  name = "robertdebock/debian:bookworm"
+resource "docker_image" "swarm_image" {
+  name         = local.docker_image_name
+  keep_locally = false
+  build {
+    context = path.module
+  }
+  triggers = {
+    dir_sha1 = sha1(join("", [
+      filesha1("${path.module}/Dockerfile")
+    ]))
+  }
 }
 
 module "swarm_containers" {
   count      = length(var.containers)
   source     = "github.com/studio-telephus/terraform-docker-container.git?ref=1.0.3"
   name       = var.containers[count.index].name
-  image      = docker_image.docker_ubuntu_systemd.image_id
+  image      = docker_image.swarm_image.image_id
   restart    = var.restart
   privileged = true
-  # entrypoint = []
   networks_advanced = [
     {
       name         = var.network_name
